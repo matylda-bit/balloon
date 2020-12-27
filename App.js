ce456b2d788e48e0188fb2785bcb474a2114c628
matylda-bit <58633008+matylda-bit@users.noreply.github.com>
2020-12-27 16:38:09 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Animated from './Animated';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="balloon-animation">
+          <Animated
+            className="flying-cloud"
+            src='/cloud.png'
+            ratioX="0.2"
+            ratioY="0.23"
+          />
+          <Animated
+            className="flying-cloud"
+            src='/cloud.png'
+            ratioX="0.26"
+            ratioY="0.34"
+          />
+          <Animated
+            className="flying-cloud"
+            src='/cloud.png'
+            ratioX="0.1"
+            ratioY="0.11"
+          />
+          <Animated
+            className="flying-cloud"
+            src='/cloud.png'
+            ratioX="0.13"
+            ratioY="0.53"
+          />
+          <Animated
+            className="flying-cloud"
+            src='/cloud.png'
+            ratioX="0.23"
+            ratioY="0.41"
+          />
+
+          <Animated
+            className="flying-balloon"
+            src='/balloon.png'
+            ratioX="0.07"
+            ratioY="0.3"
+          />
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
